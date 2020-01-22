3785d3af3667059d0214c2fe3112daf98ed1b65e
Brian Kleiner <brian.j.kleiner@gmail.com>
2020-01-22 03:09:17 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,6 +18,6 @@ def speak_to_grandma(speech)
   elsif speech != speech.upcase
     return "HUH?! SPEAK UP, SONNY!"
   else
-    return "NO, NOt SINCE 1938!"
+    return "NO, NOT SINCE 1938!"
   end
 end
